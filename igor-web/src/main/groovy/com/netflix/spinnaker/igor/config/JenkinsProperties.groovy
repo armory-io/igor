5197a0fdba8cd8c45442e0efa495c61115ee9ba4
Kevin Woo <kevinawoo@gmail.com>
2017-07-21 10:13:47 -0700

    feat(jenkins) allow polling to be turned off

--- igor-web/src/main/groovy/com/netflix/spinnaker/igor/config/JenkinsProperties.groovy
+++ igor-web/src/main/groovy/com/netflix/spinnaker/igor/config/JenkinsProperties.groovy
@@ -19,6 +19,7 @@ package com.netflix.spinnaker.igor.config
 import groovy.transform.CompileStatic
 import org.hibernate.validator.constraints.NotEmpty
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.NestedConfigurationProperty
 
 import javax.validation.Valid
 
@@ -28,6 +29,14 @@ import javax.validation.Valid
 @CompileStatic
 @ConfigurationProperties(prefix = 'jenkins')
 class JenkinsProperties {
+
+    @NestedConfigurationProperty
+    final Polling polling = new Polling()
+
+    static class Polling {
+        boolean enabled = true
+    }
+
     @Valid
     List<JenkinsHost> masters
 
